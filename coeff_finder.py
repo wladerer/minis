4919c0fd0feb65046e4e68afb5425c21ee8bb252
Will Laderer <94595045+wladerer@users.noreply.github.com>
2022-04-11 10:11:47 -0700

    turned it into a function + commented to be kind

--- coeff_finder.py
+++ coeff_finder.py
@@ -1,36 +1,37 @@
 import re
 
-file_name = '/Users/william/Desktop/Transfer/ltnf.rasscf.S1.log' #This should be the name of the RASSCF .log file
+#Filename should be the name of the RASSCF .log file
 MO_regex = r'        \b([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])  ........    ......' #Unique pattern for each individual MO
-forbs_regex = r'4f..   \(...[1-9][0-9][0-9][0-9]\)' #Unique pattern for any f-orbital component 
+orb_regex = r'..   \(...[1-9][0-9][0-9][0-9]\)' #Unique pattern for any orbital component 
 
+def find_orbs(file_name, orbital):
+    orbital = orbital + orb_regex #concatenates orbital to orb_regex string
+    with open(file_name) as f: 
+        lines = f.readlines() #checks for orbitals line by line
+        coefficients_per_line = [] #saves all coefficients per line
+        for line in lines:
+            m = re.findall(orbital, line) #finds all instances of orbitals within the current line
+            coeffs = 0
+            for occurance in m:
+                number = re.search(r'\(.(.*)\)', occurance)
+                coeffs += float(number.group(1))**2
+            coefficients_per_line.append(coeffs)
 
-with open(file_name) as f:
-    lines = f.readlines()
-    coefficients_per_line = []
-    for line in lines:
-        m = re.findall(forbs_regex, line) #finds all instances of f-orbitals, line by line
-        coeffs = 0
-        for occurance in m:
-            number = re.search(r'\(.(.*)\)', occurance)
-            coeffs += float(number.group(1))**2
-        coefficients_per_line.append(coeffs)
+    for element in range(len(coefficients_per_line)-1): #rough heuristic to add AO coeffs if on a different line but within the same MO
+        if coefficients_per_line[element+1] != 0:
+            coefficients_per_line[element] = 0
+            coefficients_per_line[element+1] += coefficients_per_line[element]
 
-for element in range(len(coefficients_per_line)-1): #rough heuristic to add AO coeffs if on a different line but within the same MO
-    if coefficients_per_line[element+1] != 0:
-        coefficients_per_line[element] = 0
-        coefficients_per_line[element+1] += coefficients_per_line[element]
+    highest_value = max(coefficients_per_line)
+    highest_index = coefficients_per_line.index(highest_value)
 
-highest_value = max(coefficients_per_line)
-highest_index = coefficients_per_line.index(highest_value)
-
-with open(file_name) as f: #extracts the coefficients from the coefficient array
-    lines = f.readlines()
-    line_count = 0
-    for line in lines:
-        if line_count == highest_index -1:
-            orbital = re.search(r'        ([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])', line)
-            print('Can be found on line ', highest_index)
-            print('Possibly orbital',orbital.group(1))
-        line_count += 1
+    with open(file_name) as f: #extracts the coefficients from the coefficient array
+        lines = f.readlines()
+        line_count = 0
+        for line in lines:
+            if line_count == highest_index -1:
+                orbital = re.search(r'        ([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])', line)
+                print('Can be found on line ', highest_index)
+                print('Possibly orbital',orbital.group(1))
+            line_count += 1
 
