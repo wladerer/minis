354f69798668705c102e536844234a6da38e58d3
wladerer <wtladerer@gmail.com>
2022-04-13 15:43:10 -0700

    prints location of the largest values

--- coeff_finder.py
+++ coeff_finder.py
@@ -1,11 +1,12 @@
 import re
+import sys
 
 #Filename should be the name of the RASSCF .log file
 MO_regex = r'        \b([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])  ........    ......' #Unique pattern for each individual MO
 orb_regex = r'..   \(...[1-9][0-9][0-9][0-9]\)' #Unique pattern for any orbital component 
 
-def find_orbs(file_name, orbital):
-    orbital = orbital + orb_regex #concatenates orbital to orb_regex string
+def coeffs_by_line(file_name):
+    orbital = '4f' + orb_regex #concatenates orbital to orb_regex string, also change functionality once you figure out how to use terminal
     with open(file_name) as f: 
         lines = f.readlines() #checks for orbitals line by line
         coefficients_per_line = [] #saves all coefficients per line
@@ -21,17 +22,35 @@ def find_orbs(file_name, orbital):
         if coefficients_per_line[element+1] != 0:
             coefficients_per_line[element] = 0
             coefficients_per_line[element+1] += coefficients_per_line[element]
+    
+    return coefficients_per_line
 
-    highest_value = max(coefficients_per_line)
-    highest_index = coefficients_per_line.index(highest_value)
 
-    with open(file_name) as f: #extracts the coefficients from the coefficient array
-        lines = f.readlines()
-        line_count = 0
-        for line in lines:
-            if line_count == highest_index -1:
-                orbital = re.search(r'        ([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])', line)
-                print('Can be found on line ', highest_index)
-                print('Possibly orbital',orbital.group(1))
-            line_count += 1
+def find_orbs(file_name):
+    coefficients = coeffs_by_line(file_name)
+    highest_values = sorted(coefficients, reverse=True) #find highest values in list
+    highest_indices = []
+    for value in highest_values:
+        highest_indices.append(coefficients.index(value)) #finds indices of highest values wrt to the original list
+    values = [i for i in highest_values if i != 0]
+    indices = [i for i in highest_indices if i != 0]
+    
+    for i,j in zip(indices, values):
+        print('The orbital found near line ' + str(round(i,5)) + ' has a contribution of: ' + str(round(j,5)))
+    
+
+
+
+    # with open(file_name) as f: #extracts the coefficients from the coefficient array
+    #     lines = f.readlines()
+    #     line_count = 0
+    #     for line in range(len(lines)):
+    #         if line_count == highest_indices[line] -1:
+    #             # orbital = re.search(r'        ([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])', line)
+    #             print('Can be found on lines ', highest_indices)
+    #             # print('Possibly orbital',orbital.group(1))
+    #         line_count += 1
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]](sys.argv[2])
 
