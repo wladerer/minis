ba61c4f68b5551d6d668fa1aa8ba920489966d07
wladerer <wtladerer@gmail.com>
2022-05-03 15:44:23 -0700

    it's not doing the thing:(

--- VASP_XML/DOS_Plotterv2.py
+++ VASP_XML/DOS_Plotterv2.py
@@ -1,4 +1,6 @@
-import json 
+from email import header
+import json
+import numpy as np 
 import xmltodict
 import pandas as pd
 
@@ -15,4 +17,29 @@ def to_json(file):
             json_file.write(json_data)
             json_file.close()
 
-data = pd.read_json("vasp_data.json")
+
+        with open('vasp_data.json','r') as f:
+            data = json.loads(f.read())
+
+
+        headers = data['modeling']['calculation']['dos']['partial']['array']['field']
+        efermi = data['modeling']['calculation']['dos']['i']['#text']
+        atoms = int(data['modeling']['atominfo']['atoms'])
+        
+        
+        indices = np.array([])
+        for i in range(1,atoms+1):
+            np.concatenate((indices, i*np.ones(301)),axis=None)
+        
+        indices = pd.DataFrame(indices)
+        
+        dos_arrays = []
+        for ion in range(atoms):
+            dos_arrays.append(data['modeling']['calculation']['dos']['partial']['array']['set']['set'][ion]['set']['r'])
+        
+        df = pd.DataFrame(np.concatenate(dos_arrays))
+        
+        df = pd.concat([df,indices])
+
+        print(df)
+to_json('VASP_XML/vasprun.xml')
