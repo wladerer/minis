644cf82dc99acb49c3e90ed1af85c5e2de9c7699
wladerer <wtladerer@gmail.com>
2022-06-24 10:46:20 -0700

    getting weird vmd errors

--- 5-DBahA_FEP/namd-files/vmd.sh
+++ 5-DBahA_FEP/namd-files/vmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
-/bin/grep ' D1 ' NRAS-DBahA-dual-top-step2.pdb > nab1.pdb
-/bin/grep ' D2 ' NRAS-DBahA-dual-top-step2.pdb > nab2.pdb
+grep ' D1' NRAS-DBahA-dual-top-step2.pdb > nab1.pdb
+grep ' D2' NRAS-DBahA-dual-top-step2.pdb > nab2.pdb
 vmd -dispdev text -e psfgen-dual-topology.tcl
+vmd -psf dna.psf -pdb dna.pdb
+
+vmd -dipsdev text -e vmd.inp 
